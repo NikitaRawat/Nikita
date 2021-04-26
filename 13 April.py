7a171c06f5b86f53cffa541cd29fad241d85ecfc
NikitaRawat <nikitarawat636@gmail.com>
2021-04-26 15:52:56 +0530

    first commit

---
+++ 13 April.py
@@ -0,0 +1,35 @@
+##a=int(input("Enter any number "))
+##if a>50:
+##  print("Congratulations!")
+##elif a==50:
+##    print ("Your score is 50")
+##else :
+##  print("You are fail!")
+
+
+
+
+
+##a=[1,2,3,4,5,6,7,8]
+##for i in a:
+##    print (i)
+##    if i==5:
+##        pass
+
+##a=1
+##while a<20:
+##    print(a)
+##    a=a+1
+##    if a==10:
+##        break
+
+a=int(input("Enter any number "))
+b=int(input("Enter any second number "))
+print(a+b)
+
+a=int(input("Enter any number "))
+b=int(input("Enter any second number "))
+print(a+b)
+
+
+  
